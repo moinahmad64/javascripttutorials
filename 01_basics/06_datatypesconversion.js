e6f412a642b8618e00afecdcf236d6fa9312bee4
moinahmad64 <146588574+moinahmad64@users.noreply.github.com>
2026-04-06 13:47:25 +0000

    container config done

---
+++ 01_basics/06_datatypesconversion.js
@@ -0,0 +1,15 @@
+
+/*let score = 33
+let numberString = String(score)
+console.log(score);
+console.log(typeof score);
+console.log(typeof numberString);*/
+
+/*let loggenIn = true;
+let LoggedInString = String(loggenIn)
+
+console.log(loggenIn);
+console.log(typeof loggenIn);
+console.log(LoggedInString);
+console.log(typeof LoggedInString);*/
+
