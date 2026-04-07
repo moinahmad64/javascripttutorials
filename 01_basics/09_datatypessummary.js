d060da484ec4a5f0708b6f5525b270ec0ec7ca70
moinahmad64 <146588574+moinahmad64@users.noreply.github.com>
2026-04-07 13:52:25 +0000

    container config done

---
+++ 01_basics/09_datatypessummary.js
@@ -0,0 +1,32 @@
+
+
+/* 
+
+Primitive call by value
+ string number boolean null undefined symbol big-int
+ 
+*/
+
+/* reference type non primitive
+
+array objects functions
+
+*/      
+
+
+let dataTypeString = "Moin"
+let dataTypeNumber = 2026
+let dataTypeBoolean = false
+let dataTypeNull = null
+let dataTypeUndefined = 
+//let dataTypeSymbol = Symbol('123')
+//let dataTypeBigint = 4646445445444464n
+
+console.log(typeof dataTypeString);
+console.log(typeof dataTypeNumber);
+console.log(typeof dataTypeBoolean);
+console.log(typeof dataTypeNull);
+console.log(typeof dataTypeUndefined);
+//console.log(typeof dataTypeSymbol);
+//console.log(typeof dataTypeBigint);
+
