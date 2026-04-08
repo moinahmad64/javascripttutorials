7c12377c904f56b3c92b146192cb2658b8027c15
moinahmad64 <146588574+moinahmad64@users.noreply.github.com>
2026-04-08 13:44:28 +0000

    container config done

---
+++ 01_basics/11_strings.js
@@ -0,0 +1,34 @@
+
+const devName = "Moin"
+const repoCount = 25
+
+//console.log(`My name is ${devName} and my repo count is ${repoCount}`)
+
+const gameName = new String('pubg-mobile')
+
+//console.log(gameName)
+//console.log(gameName[2])
+//console.log(`my name is ${devName}. my repocount is ${repoCount}. i like to play ${gameName}`)
+
+//console.log(gameName.toUpperCase())
+//console.log(gameName[2].toUpperCase())
+//console.log(gameName.charAt(2))
+//console.log(gameName.indexOf('u'))
+
+//const firstString = gameName.substring(0,4)
+//console.log(newString)
+
+//const secondString = gameName.slice(0,4)
+//console.log(secondString)
+
+//const thirdString = "   ahmad   "
+//console.log(thirdString)
+//console.log(thirdString.trim())
+
+//const url = "https://moin.com/moin%20ahmad"
+//console.log(url.replace('%20','-'))
+//console.log(url.includes('moin'))
+//console.log(url.includes('ali'))
+
+//const fourthString = "one-two-three-four-five"
+//console.log(fourthString.split('-'))
