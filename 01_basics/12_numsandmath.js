4fd593b09cc6d9713205135f330632b1dcf1ca66
moinahmad64 <146588574+moinahmad64@users.noreply.github.com>
2026-04-09 13:41:57 +0000

    container config done

--- 01_basics/12_numsandmath.js
+++ 01_basics/12_numsandmath.js
@@ -2,8 +2,8 @@
 const firstNumber = 400
 const secondNumber = new Number(100)
 
-console.log(firstNumber)
-console.log(secondNumber)
+//console.log(firstNumber)
+//console.log(secondNumber)
 
 //console.log(secondNumber.toString())
 //console.log(typeof(secondNumber.toString()))
@@ -11,10 +11,31 @@ console.log(secondNumber)
 
 //console.log(secondNumber.toFixed(2))
 
-const thirdNumber = 123.8966
+//const thirdNumber = 123.8966
 
 //console.log(thirdNumber.toPrecision(4))
 
-const fourthNumber = 1000000
+//const fourthNumber = 1000000
 
 //console.log(fourthNumber.toLocaleString('en-IN'))
+
+//=================MATH===================
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.6));
+//console.log(Math.min(2, 4, 7, 8));
+//console.log(Math.max(2, 4, 7, 8));
+
+//console.log(Math.random() * 10);
+//console.log(Math.round(Math.random() * 10));
+
+const min = 11
+const max = 20
+
+console.log(Math.round(Math.random() * ((max - min) ) + min))
+
+
+
