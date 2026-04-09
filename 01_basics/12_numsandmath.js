b05995630471a5c472baab39e43a006045780882
moinahmad64 <146588574+moinahmad64@users.noreply.github.com>
2026-04-09 12:26:40 +0000

    container config done

---
+++ 01_basics/12_numsandmath.js
@@ -0,0 +1,20 @@
+
+const firstNumber = 400
+const secondNumber = new Number(100)
+
+console.log(firstNumber)
+console.log(secondNumber)
+
+//console.log(secondNumber.toString())
+//console.log(typeof(secondNumber.toString()))
+//console.log(secondNumber.toString().length)
+
+//console.log(secondNumber.toFixed(2))
+
+const thirdNumber = 123.8966
+
+//console.log(thirdNumber.toPrecision(4))
+
+const fourthNumber = 1000000
+
+//console.log(fourthNumber.toLocaleString('en-IN'))
