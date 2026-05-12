72c41815bed80ce6833b08d55e782542e41ab09f
moinahmad64 <146588574+moinahmad64@users.noreply.github.com>
2026-05-12 13:10:42 +0000

    container config done

---
+++ 03_basics/27_forloop.js
@@ -0,0 +1,12 @@
+
+// FOR LOOP
+
+for (let i = 1; i <=10; i++) {
+    const element = i;
+    console.log(`Table of ${i}`)
+    for (let j = 1; j <= 10 ; j++) {
+        const element = j;
+        console.log(`${i} * ${j} = ${i * j}`)
+    }
+    
+}
