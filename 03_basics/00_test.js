5f0f3ae9ac5415cc5cb9ddb3286f7ec410b1fa5c
moinahmad64 <146588574+moinahmad64@users.noreply.github.com>
2026-05-14 13:53:17 +0000

    container config done

---
+++ 03_basics/00_test.js
@@ -0,0 +1,12 @@
+
+
+for (let i = 1; i <=10; i++) {
+    const element = i;
+    console.log(`Table of ${i}`)
+    for (let j = 1; j <= 10; j++) {
+        const element = j;
+        console.log(`${i} * ${j} = ${i * j}`)
+        
+    }
+    
+}
---
+++ 03_basics/29_highorderarray.js
@@ -0,0 +1,81 @@
+
+
+//["", "", ""]
+//[{}, {},{}]
+
+const firstArray = [1, 2, 3, 4, 5]
+
+for (const num of firstArray) {
+   // console.log(num)
+    
+}
+
+//============================
+
+const greeting = "hello world"
+for (const greet of greeting) {
+   // console.log(`$ char of greeting "${greet}"`)
+    
+}
+
+//============================
+
+// MAP
+
+const map = new Map()
+
+map.set('pk', "Pakistan")
+map.set('uk', "United Kingdom")
+map.set('usa', "United States of America")
+
+//console.log(map)
+
+//for (const key of map) {
+//    console.log(key)    
+//}
+
+//for (const [key, value] of map) {
+//    console.log(key , value)    
+//}
+
+
+//======================================
+
+const games = {
+    'game1': 'gta',
+    'game2': 'nfs'
+}
+
+//for (const [key, value] of games) {
+   // console.log(key, value)    
+//}
+
+const games2 = {
+    game1: 'gta',
+    game2: 'nfs'
+}
+
+//for (const [key, value] of games2) {
+    //console.log(key, value)    
+//}
+
+//============================================
+
+const myLangs = {
+    js: 'javascript',
+    html: 'hyper text markup lang',
+    css: 'cascading style sheet',
+    cpp: 'c++'
+}
+
+//for (const key in myLangs) {
+//    console.log(key);
+//}
+
+//for (const key in myLangs) {
+//    console.log(`${key} is shortcut for ${myLangs[key]}`)
+//}
+
+//===========================================
+
+const programmingLangs = ["js", "java", "c++", "python"]
